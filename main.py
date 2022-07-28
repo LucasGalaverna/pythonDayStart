eb91a3ef4be9a06cb8fd843b64d0127885a167cb
Lucas Galaverna <98967463+LucasGalaverna@users.noreply.github.com>
2022-07-28 11:28:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pyperclip
 import time
 
 
-## Programa p/ iniciar o google chorme e deixar em pronto emprego as abas mais utilizadas
+## Programa p/ iniciar o google chorme e deixar em pronto emprego as abas mais utilizadas. 
+## Abre também o Sistema específico da empresa realizando login com senha. 
 
 pyautogui.PAUSE = 1
 
@@ -15,16 +16,17 @@ pyautogui.click(x=983, y=30)
 pyperclip.copy("https://g1.globo.com/")
 pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
-time.sleep(1.1)
+time.sleep(1.5)
 pyautogui.hotkey("ctrl", "t")
 pyperclip.copy("https://app.pinkapp.com/home")
 pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
-time.sleep(1.2)
+time.sleep(1.5)
 pyautogui.hotkey("ctrl", "t")
 pyperclip.copy("https://www.websoma.com.br/somaWeb/pages/orcamentoVistoria/orcamentoVistoria.xhtml")
 pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
+time.sleep(1.5)
 pyautogui.press("win")
 pyautogui.write("live")
 time.sleep(1.5)
