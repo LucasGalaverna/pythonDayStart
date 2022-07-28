243eb0a30ff34a15ba8dc6097af02d89159b16dc
LucasGalaverna <galaverna.536@gmail.com>
2022-07-28 11:17:25 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pyautogui
+import pyperclip
+import time
+
+
+## Programa p/ iniciar o google chorme e deixar em pronto emprego as abas mais utilizadas
+
+pyautogui.PAUSE = 1
+
+pyautogui.press("win")
+pyperclip.write("chrome")
+pyautogui.press("enter")
+time.sleep(1.2)
+pyautogui.click(x=983, y=30)
+pyperclip.copy("https://g1.globo.com/")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(1.1)
+pyautogui.hotkey("ctrl", "t")
+pyperclip.copy("https://app.pinkapp.com/home")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(1.2)
+pyautogui.hotkey("ctrl", "t")
+pyperclip.copy("https://www.websoma.com.br/somaWeb/pages/orcamentoVistoria/orcamentoVistoria.xhtml")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+pyautogui.press("win")
+pyautogui.write("live")
+time.sleep(1.5)
+pyautogui.click(x=159, y=115)
+pyautogui.press("enter")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.click(x=575, y=327)
+pyperclip.copy("lg2606")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.click(x=802, y=394)
+
